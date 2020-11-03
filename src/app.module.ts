05e7a55dafb92c493b7b4955aed7425c42a84eb2
Dejan Erjavec <dejaner@gmail.com>
2020-11-03 20:40:49 +0100

    Refactore DB config to url

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,11 +14,7 @@ import { Url } from './url/url.entity';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get('DB_USER'),
-        password: configService.get('DB_PASS'),
-        database: configService.get('DB_NAME'),
+        url: configService.get('DATABASE_URL'),
         entities: [Url],
         synchronize: true
       }),
